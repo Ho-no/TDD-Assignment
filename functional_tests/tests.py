c602d635d49eb4b60cce4f78027a5027441f49ac
Ho-no <Hono_sama@qq.com>
2022-05-24 15:23:57 +0800

    WIP

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -67,11 +67,10 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-
+        # self.fail('Finish the test!')
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-        self.fail('Finish the test!')
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
